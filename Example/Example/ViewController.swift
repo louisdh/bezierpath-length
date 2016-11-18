6c5d7568053161f259566b8aac9f5de5e9b35374
Louis D'hauwe <louisdhauwe@silverfox.be>
2016-11-18 23:38:49 +0100

    Project setup

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Louis D'hauwe on 18/11/2016.
+//  Copyright © 2016 Silver Fox. All rights reserved.
+//
+
+import UIKit
+import BezierPathLength
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
