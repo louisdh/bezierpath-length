eb052f6af711dd7830cea852823937f5e4ab4970
Louis D'hauwe <louisdhauwe@silverfox.be>
2016-11-20 17:04:24 +0100

    Added support for macOS

--- Source/BezierPath+Length.swift
+++ Source/BezierPath+Length.swift
@@ -17,6 +17,36 @@ import CoreGraphics
 	typealias BezierPath = UIBezierPath
 #endif
 
+
+#if os(macOS)
+
+	// http://stackoverflow.com/questions/1815568/how-can-i-convert-nsbezierpath-to-cgpath
+	extension NSBezierPath {
+		
+		public var cgPath: CGPath {
+			let path = CGMutablePath()
+			var points = [CGPoint](repeating: .zero, count: 3)
+			
+			for i in 0 ..< self.elementCount {
+				let type = self.element(at: i, associatedPoints: &points)
+				switch type {
+				case .moveToBezierPathElement:
+					path.move(to: points[0])
+				case .lineToBezierPathElement:
+					path.addLine(to: points[0])
+				case .curveToBezierPathElement:
+					path.addCurve(to: points[2], control1: points[0], control2: points[1])
+				case .closePathBezierPathElement:
+					path.closeSubpath()
+				}
+			}
+			
+			return path
+		}
+	}
+	
+#endif
+
 public extension BezierPath {
 
 	/// Length of path in pt
