4e80494b9ebac47afe7009b35ccb863d4489360d
Louis D'hauwe <louisdhauwe@silverfox.be>
2016-11-23 22:32:04 +0100

    Added watchOS and tvOS framework targets

---
+++ BezierPathLength tvOS/BezierPathLength tvOS.h
@@ -0,0 +1,19 @@
+//
+//  BezierPathLength tvOS.h
+//  BezierPathLength tvOS
+//
+//  Created by Louis D'hauwe on 23/11/2016.
+//  Copyright © 2016 Silver Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BezierPathLength tvOS.
+FOUNDATION_EXPORT double BezierPathLength_tvOSVersionNumber;
+
+//! Project version string for BezierPathLength tvOS.
+FOUNDATION_EXPORT const unsigned char BezierPathLength_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BezierPathLength_tvOS/PublicHeader.h>
+
+
